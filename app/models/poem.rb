6abe77f37267b7c7afd2afaa4c1930fd3c1fad1d
erica lai <ericalai89@gmail.com>
2016-06-25 21:33:12 +0000

    validate user input on form

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,2 +1,8 @@
 class Poem < ActiveRecord::Base
+  validates :noun, presence:true, length: {minimum:3, maximum:10}
+  validates :adverb, presence:true, length: {minimum:3, maximum:50}
+  validates :pluralnoun, presence:true, length: {minimum:3, maximum:50}
+  validates :verb, presence:true, length: {minimum:3, maximum:50}
+  validates :adjective, presence:true, length: {minimum:3, maximum:50}
+  validates :plant, presence:true, length: {minimum:3, maximum:50}
 end
