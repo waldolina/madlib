be4f910dfb77aa4824199f0099bc2e4f92ee72c0
erica lai <ericalai89@gmail.com>
2016-06-25 21:30:05 +0000

    added simpleform and made submit button work

--- Gemfile
+++ Gemfile
@@ -49,3 +49,5 @@ gem 'rails_12factor', group: :production
 
 gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'sprockets', '2.11.0'
+
+gem 'simple_form'
--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -2,4 +2,20 @@ class PoemsController < ApplicationController
   def index
     @poem = Poem.first
   end
+
+  def new
+    @poem = Poem.new
+  end
+
+  def create
+    @poem = Poem.create(poem_params)
+    redirect_to root_path
+  end
+end
+
+
+private
+
+def poem_params
+  params.require(:poem).permit(:noun, :adverb, :pluralnoun, :verb, :adjective, :plant)
 end
